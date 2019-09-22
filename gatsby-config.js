8f30f441cc8acfab6727e6a07a11525d7537b3bd
Adams Alves <alves.adams@gmail.com>
2019-09-22 14:58:55 -0300

    change name: plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Adams Alves - Web Designer e Front-end`,
+        name: `Adams Alves - Dev. Front-end`,
         short_name: `Adams Alves`,
         start_url: `/`,
         background_color: `#ffffff`,
